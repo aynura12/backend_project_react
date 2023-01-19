91c0558b85dcd08f82673aa7aa3d43b306ba6521
aynura12 <7m1bz3t@coe.du.az>
2023-01-19 09:27:43 -0800

    new commit

--- fullstack/frontend/src/component/Home.jsx
+++ fullstack/frontend/src/component/Home.jsx
@@ -5,7 +5,6 @@ import axios from "axios"
 import "../style/home.css"
 const Home = () => {
   const [products, setProducts] = useState(null);
-
   const getData = async () => {
     const res =await axios.get("http://localhost:8080/products");
     setProducts(res.data.data);
@@ -15,13 +14,18 @@ const Home = () => {
     getData(); 
   }, []);
 
+  const deleteData = (id) => {
+    axios.delete(`http://localhost:8080/products${id}`);
+    getData();
+  };
   return (
     <div className="container">
-        {products?.map((product,index)=>{
-            return(<div className="mycard"  key={index}><img src={product.picture}></img>
-            <h4>{product.name}</h4>
-            <p>${product.price}</p>
-            <button>View details</button></div>)
+        {products?.map((product)=>{
+            return(<div className="mycard"  key={product.id}><img src={product.picture}></img>
+            <h4>{product?.name}</h4>
+            <p>${product?.price}</p>
+            <button>View details</button>
+            <button onClick={(id)=>deleteData(product.id)}>Delete</button></div>)
         })}
       
      
